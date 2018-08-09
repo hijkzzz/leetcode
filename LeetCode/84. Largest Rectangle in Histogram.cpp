98be350bda57821a7953cf9f8d0e8b2227c6f61e
hijkzzz <janhu9527@gmail.com>
2018-08-09 18:32:08 +0800

    2018.8.9

---
+++ LeetCode/84. Largest Rectangle in Histogram.cpp
@@ -0,0 +1,28 @@
+// 递增区间用栈来保存，然后遍历这个区间可能的最大值
+// 对于非递增区间，直接计算可能的最大值
+ 
+ class Solution {
+    public:
+        int largestRectangleArea(vector<int> &height) {
+            
+            int ret = 0;
+            height.push_back(0);
+            vector<int> index;
+            
+            for(int i = 0; i < height.size(); i++)
+            {
+                while(index.size() > 0 && height[index.back()] >= height[i])
+                {
+                    int h = height[index.back()];
+                    index.pop_back();
+                    
+                    int sidx = index.size() > 0 ? index.back() : -1;
+                    if(h * (i-sidx-1) > ret)
+                        ret = h * (i-sidx-1);
+                }
+                index.push_back(i);
+            }
+            
+            return ret;
+        }
+    };
