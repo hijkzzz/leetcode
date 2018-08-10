8db71a0a706bb56e0e3088da92b9c3ed9e5ac252
hijkzzz <janhu9527@gmail.com>
2018-08-11 06:49:37 +0800

    add

---
+++ LeetCode/140. Word Break II.java
@@ -0,0 +1,24 @@
+public List<String> wordBreak(String s, Set<String> wordDict) {
+    return DFS(s, wordDict, new HashMap<String, LinkedList<String>>());
+}       
+
+// DFS function returns an array including all substrings derived from s.
+List<String> DFS(String s, Set<String> wordDict, HashMap<String, LinkedList<String>>map) {
+    if (map.containsKey(s)) 
+        return map.get(s);
+        
+    LinkedList<String>res = new LinkedList<String>();     
+    if (s.length() == 0) {
+        res.add("");
+        return res;
+    }               
+    for (String word : wordDict) {
+        if (s.startsWith(word)) {
+            List<String>sublist = DFS(s.substring(word.length()), wordDict, map);
+            for (String sub : sublist) 
+                res.add(word + (sub.isEmpty() ? "" : " ") + sub);               
+        }
+    }       
+    map.put(s, res);
+    return res;
+}
